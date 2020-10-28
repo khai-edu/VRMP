cf44ce33701afeb061fabd1a9d12b07fd19f34be
Alexey Chernykh <uspiritwolf@outlook.com>
2020-10-28 18:39:21 +0200

    - formated code

--- Assets/Scripts/CameraPointer.cs
+++ Assets/Scripts/CameraPointer.cs
@@ -24,39 +24,44 @@ using UnityEngine;
 /// </summary>
 public class CameraPointer : MonoBehaviour
 {
-    private const float _maxDistance = 10;
-    private GameObject _gazedAtObject = null;
+	private const float _maxDistance = 10;
+	private GameObject _gazedAtObject = null;
 
-    /// <summary>
-    /// Update is called once per frame.
-    /// </summary>
-    public void Update()
-    {
-        // Casts ray towards camera's forward direction, to detect if a GameObject is being gazed
-        // at.
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit, _maxDistance))
-        {
-            // GameObject detected in front of the camera.
-            if (_gazedAtObject != hit.transform.gameObject)
-            {
-                // New GameObject.
-                _gazedAtObject?.SendMessage("OnPointerExit", SendMessageOptions.DontRequireReceiver);
-                _gazedAtObject = hit.transform.gameObject;
-                _gazedAtObject.SendMessage("OnPointerEnter", SendMessageOptions.DontRequireReceiver);
-            }
-        }
-        else
-        {
-            // No GameObject detected in front of the camera.
-            _gazedAtObject?.SendMessage("OnPointerExit", SendMessageOptions.DontRequireReceiver);
-            _gazedAtObject = null;
-        }
+	/// <summary>
+	/// Update is called once per frame.
+	/// </summary>
+	public void Update()
+	{
+		if (!Google.XR.Cardboard.Api.HasDeviceParams())
+		{
+			return;
+		}
 
-        // Checks for screen touches.
-        if (Google.XR.Cardboard.Api.IsTriggerPressed)
-        {
-            _gazedAtObject?.SendMessage("OnPointerClick", SendMessageOptions.DontRequireReceiver);
-        }
-    }
+		// Casts ray towards camera's forward direction, to detect if a GameObject is being gazed
+		// at.
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, transform.forward, out hit, _maxDistance))
+		{
+			// GameObject detected in front of the camera.
+			if (_gazedAtObject != hit.transform.gameObject)
+			{
+				// New GameObject.
+				_gazedAtObject?.SendMessage("OnPointerExit", SendMessageOptions.DontRequireReceiver);
+				_gazedAtObject = hit.transform.gameObject;
+				_gazedAtObject.SendMessage("OnPointerEnter", SendMessageOptions.DontRequireReceiver);
+			}
+		}
+		else
+		{
+			// No GameObject detected in front of the camera.
+			_gazedAtObject?.SendMessage("OnPointerExit", SendMessageOptions.DontRequireReceiver);
+			_gazedAtObject = null;
+		}
+
+		// Checks for screen touches.
+		if (Google.XR.Cardboard.Api.IsTriggerPressed)
+		{
+			_gazedAtObject?.SendMessage("OnPointerClick", SendMessageOptions.DontRequireReceiver);
+		}
+	}
 }
