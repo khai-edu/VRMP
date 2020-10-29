ab5f57743fd2f8c413a8ca8b85a17e196ac70161
efosamark <efosamark@gmail.com>
2020-10-29 23:43:12 +0200

    Reworked prefabs: added working Marker and Whiteboard prefabs. Added XR Rig to the scene

---
+++ Assets/Scripts/WB/Marker.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Marker : MonoBehaviour
+{
+    protected Rigidbody mRigidbody;
+    private Vector3 startPosition;
+    private Quaternion starRotation;
+
+    [SerializeField]
+    private Color color;
+
+    [SerializeField]
+    private MeshRenderer[] colouredParts;
+
+
+    [SerializeField]
+    private Painter painter;
+
+    [SerializeField]
+    private PaintReceiver paintReceiver;
+
+    protected void Awake()
+    {
+        mRigidbody = GetComponent<Rigidbody>();
+
+        startPosition = mRigidbody.position;
+        starRotation = mRigidbody.rotation;
+
+        foreach (MeshRenderer renderer in colouredParts)
+        {
+            renderer.material.color = color;
+        }
+
+        painter.Initialize(paintReceiver);
+        painter.ChangeColour(color);
+    }
+}
