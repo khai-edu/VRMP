d3e09f84fb2578cdc92e806d6a1da357a553d21d
Alexey Chernykh <uspiritwolf@outlook.com>
2020-11-01 23:35:18 +0200

    + added Photon Parent View

---
+++ Assets/Scripts/PhotonParentView.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonParentView : MonoBehaviourPun, IPunObservable
+{
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.IsWriting)
+		{
+			string parentName = this.gameObject.transform.parent.name;
+			stream.SendNext(parentName);
+		}
+		else
+		{
+			string parentName = (string)stream.ReceiveNext();
+			var parent = GameObject.Find(parentName);
+			this.gameObject.transform.SetParent(parent.transform);
+		}
+	}
+}
